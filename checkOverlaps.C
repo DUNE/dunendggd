1cf681dd857c5c4e23b12c541781cc1a8a1d97fe
Palash Roy <pkroy@dunegpvm04.fnal.gov>
2022-12-08 15:17:47 -0600

    checkOverlap have standard method and sampling method

--- checkOverlaps.C
+++ checkOverlaps.C
@@ -4,27 +4,43 @@ void checkOverlaps(TString filename)
 	geo->Import(filename);
 
 	cout<<"======================== Checking Geometry ============================="<<endl;
-	geo->CheckGeometry();
-	cout<<"========================       Done!       ============================="<<endl;
-
+	//geo->CheckGeometry();
+	//cout<<"========================       Done!       ============================="<<endl;
 	
-	cout<<"======================== Checking Overlaps ============================="<<endl;
-	geo->CheckOverlaps(1e-5,"s");
+	cout<<"======================== Checking Overlaps with Standard Method ============================="<<endl;
+	geo->CheckOverlaps(1e-6);
 	geo->PrintOverlaps();
 	cout<<"========================       Done!       =============================\n\n\n"<<endl;
-
-	TObjArray* overlaps=geo->GetListOfOverlaps();
-	for(int i=0; i<overlaps->GetEntries(); i++){
-	  TObject* overlap=overlaps->At(i);
+	TObjArray* overlaps_standard=geo->GetListOfOverlaps();
+	for(int i=0; i<overlaps_standard->GetEntries(); i++){
+	  TObject* overlap=overlaps_standard->At(i);
 	  cout<<"========================  Drawing Overlaps ============================="<<endl;
 	  cout<<"================= Overlap messages will duplicate below ================"<<endl;
 	  cout<<"=================     Overlaps are in units of cm       ================"<<endl;
-	  TCanvas* c = new TCanvas();
+	  TCanvas* c_standard = new TCanvas("c_standard","standard method");
+	  overlap->Draw();
+	  TCanvas* p = new TCanvas("p","standard method_1");
+	  overlap->Draw("ogl");
+	  
+	  cout<<"========================       Done!       =============================\n\n\n"<<endl;
+	  
+	  }
+	/*cout<<"======================== Checking Overlaps with samplig method ============================="<<endl;
+       	geo->CheckOverlaps(1e-6,"s");
+	geo->PrintOverlaps();
+	cout<<"========================       Done!       =============================\n\n\n"<<endl;
+	TObjArray* overlaps_sampled=geo->GetListOfOverlaps();
+	for(int i=0; i<overlaps_sampled->GetEntries(); i++){
+	  TObject* overlap=overlaps_sampled->At(i);
+	  cout<<"========================  Drawing Overlaps ============================="<<endl;
+	  cout<<"================= Overlap messages will duplicate below ================"<<endl;
+	  cout<<"=================     Overlaps are in units of cm       ================"<<endl;
+	  TCanvas* c_sampled = new TCanvas("c_sampled","sampling method");
 	  overlap->Draw("");
-	  TCanvas* cogl = new TCanvas();
+	  TCanvas* k = new TCanvas("k","sampling method" );
 	  overlap->Draw("ogl");
 	  cout<<"========================       Done!       =============================\n\n\n"<<endl;
 	  
-	}
+	  }*/
 
 }
