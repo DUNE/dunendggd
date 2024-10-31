616314ad4041bb5a100b6fb519db9dd78807dd2a
Asa Nehm <pnehm@dunegpvm08.fnal.gov>
2024-10-31 12:54:12 -0500

    Setting TMS to stereo layout

--- duneggd/SubDetector/TMS.py
+++ duneggd/SubDetector/TMS.py
@@ -405,7 +405,7 @@ class tmsBuilder(gegede.builder.Builder):
         thin_Modlayer_pla = [geom.structure.Placement('l',volume=Module_layer_lv1,pos=thinModlayer_pos[1])]*Module_layers_thin
         # thin_Modlayer_pla2 = [geom.structure.Placement('k',volume=Module_layer_lv2,pos=thinModlayer_pos[2])]*Module_layers_thin
 
-        hybrid = True
+        hybrid = False
     
         for module in range(Module_layers_thin):
             zpos = -Q("3.650m") -Q("0.0325m") + module * Q("0.065m")   
