7c4f0c552df6a5ac2801b3744a0e8f8679cb9a2d
kyjung <dmsrl1100@gmail.com>
2025-07-02 23:09:30 -0400

    Fix options

--- geoDisplay.C
+++ geoDisplay.C
@@ -15,6 +15,6 @@ void geoDisplay(TString filename, Int_t VisLevel=5)
 	v->SetStyle(TGLRnrCtx::kOutline);
 	v->SetSmoothPoints(kTRUE);
 	v->SetLineScale(0.5);
-  //	v->UseDarkColorSet();
+	//	v->UseDarkColorSet();
 	v->UpdateScene();
 }
