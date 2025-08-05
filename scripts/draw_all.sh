cd74680318c5fcf31fd2e2e8ef9fe05093b41d76
Lukas Koch <lukas.koch@mailbox.org>
2025-08-05 17:29:52 +0200

    Make plotting script use SNAP root install if available.

--- scripts/draw_all.sh
+++ scripts/draw_all.sh
@@ -1,22 +1,34 @@
+#!/bin/bash
 scriptname=draw_geometry.py
+
+# Get python to use
+if which root-framework.pyroot 1>/dev/null 2>/dev/null
+then
+    # Use SNAP package root if available
+    PYTHON=root-framework.pyroot
+else
+    # Default to `python`
+    PYTHON=python
+fi
+
 if [[ $# -eq 0 ]] || [[ "$1" == "--help" ]] || [[ "$1" == "-h" ]]; then
   echo "Usage ./draw_all.sh --filename <gdml file> + any additional args"
   echo "Automatically draws all default views"
-  echo "Here's the output of python $scriptname --help:"
-  python $scriptname --help
+  echo "Here's the output of $PYTHON $scriptname --help:"
+  $PYTHON $scriptname --help
   exit 0
 fi
-python $scriptname --view xz_zoom $@
-python $scriptname --view yz_zoom $@
-python $scriptname --view sand_top $@
-python $scriptname --view sand_side $@
-python $scriptname --view lar_side $@
-python $scriptname --view lar_top $@
-python $scriptname --view rock_top $@
-python $scriptname --view rock_side $@
-python $scriptname --view tms_side $@
-python $scriptname --view tms_top $@
-python $scriptname --view xz $@
-python $scriptname --view xz_wide $@
-python $scriptname --view yz $@
-python $scriptname --view yz_wide $@
+$PYTHON $scriptname --view xz_zoom $@
+$PYTHON $scriptname --view yz_zoom $@
+$PYTHON $scriptname --view sand_top $@
+$PYTHON $scriptname --view sand_side $@
+$PYTHON $scriptname --view lar_side $@
+$PYTHON $scriptname --view lar_top $@
+$PYTHON $scriptname --view rock_top $@
+$PYTHON $scriptname --view rock_side $@
+$PYTHON $scriptname --view tms_side $@
+$PYTHON $scriptname --view tms_top $@
+$PYTHON $scriptname --view xz $@
+$PYTHON $scriptname --view xz_wide $@
+$PYTHON $scriptname --view yz $@
+$PYTHON $scriptname --view yz_wide $@
